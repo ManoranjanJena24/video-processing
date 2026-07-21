26750da9ee6e15914d2ecd8ed75a34cfa29e792b
Manoranjan Jena <manoranjanjenamitsy17@gmail.com>
2026-07-21 15:22:46 +0530

    Added to Job added to BullMQ.

--- backend/src/controllers/uploadController.js
+++ backend/src/controllers/uploadController.js
@@ -1,23 +1,9 @@
-// const uploadVideo = (req, res) => {
-//   console.log(req.file);
-
-//   res.status(200).json({
-//     success: true,
-//     message: "Video uploaded successfully",
-//     file: req.file,
-//   });
-// };
-
-// module.exports = {
-//   uploadVideo,
-// };
-
-
-
 const Video = require("../models/Video");
+const videoQueue = require("../queues/videoQueue");
 
 const uploadVideo = async (req, res) => {
   try {
+    // Save video metadata to MongoDB
     const video = await Video.create({
       originalName: req.file.originalname,
       filename: req.file.filename,
@@ -25,16 +11,30 @@ const uploadVideo = async (req, res) => {
       status: "Uploaded",
     });
 
+    // Add job to Redis queue
+    await videoQueue.add("transcode-video", {
+      videoId: video._id.toString(),
+    });
+
+    // Update status
+    video.status = "Queued";
+    await video.save();
+
     res.status(201).json({
       success: true,
+      message: "Video uploaded and added to queue successfully",
       video,
     });
   } catch (error) {
-    console.log(error); // Important
+    console.log(error);
+
     res.status(500).json({
+      success: false,
       message: error.message,
     });
   }
 };
 
-module.exports = { uploadVideo };
+module.exports = {
+  uploadVideo,
+};
--- backend/src/routes/uploadRoute.js
+++ backend/src/routes/uploadRoute.js
@@ -6,7 +6,6 @@ const upload = require("../middlewares/upload");
 
 const { uploadVideo } = require("../controllers/uploadController");
 
-// POST /upload
 router.post("/upload", upload.single("video"), uploadVideo);
 
 module.exports = router;
